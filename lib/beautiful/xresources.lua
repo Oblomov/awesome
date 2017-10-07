d3f59f9cb92611f73c20f05abcb759a50333a74b
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2017-10-07 23:19:08 +0200

    xresources: differentiate between user-set and autodetected DPI
    This allows us to cache autocomputed DPI values so that they can be
    recomputed as appropriate, without overwriting user preferences.

--- lib/beautiful/xresources.lua
+++ lib/beautiful/xresources.lua
@@ -64,6 +64,9 @@ function xresources.get_current_theme()
 end
 
 
+--- Store the cached or user-set per-screen DPI
+-- Values are tables with key dpi (the dpi) and set_from (user, auto, xrdb)
+-- The same is done for xresources.dpi
 local dpi_per_screen = {}
 
 local dpi_scale_rounding = nil
@@ -81,40 +84,81 @@ local function rounded_dpi(dpi)
     end
 end
 
+--- Compute the global DPI from the core protocol information
+local function set_global_dpi()
+    xresources.dpi = nil
+    -- Might not be present when run under unit tests
+    if awesome and awesome.xrdb_get_value then
+        local xft_dpi = tonumber(awesome.xrdb_get_value("", "Xft.dpi"))
+        if xft_dpi then
+            xresources.dpi = {
+                dpi = xft_dpi,
+                set_from = 'xrdb'
+            }
+        end
+    end
+    -- Following Keith Packard's whitepaper on Xft,
+    -- https://keithp.com/~keithp/talks/xtc2001/paper/xft.html#sec-editing
+    -- the proper fallback for Xft.dpi is the vertical DPI reported by
+    -- the X server. This will generally be 96 on Xorg, unless the user
+    -- has configured it differently
+    if not xresources.dpi then
+        if root then
+            local mm_to_inch = 25.4
+            local _, h = root.size()
+            local _, hmm = root.size_mm()
+            if hmm ~= 0 then
+                xresources.dpi = {
+                    dpi = rounded_dpi(h*mm_to_inch/hmm),
+                    set_from = 'auto'
+                }
+            end
+        end
+    end
+    -- ultimate fallback
+    if not xresources.dpi then
+        xresources.dpi = { dpi = 96, set_from = 'auto' }
+    end
+end
+
+
+--- Recompute autodetected DPI values
+--
+-- This function should be invoked whenever automatically-compued DPI might have
+-- changed (e.g. RANDR signal, or DPI-rounding function changes)
+local function recalc_auto_dpi()
+    if xresources.dpi and xresources.dpi.set_from == 'auto' then
+        set_global_dpi()
+    end
+    for s, dpi in pairs(dpi_per_screen) do
+        if dpi.set_from == 'auto' then
+            dpi_per_screen[s] = {
+                dpi = rounded_dpi(s.dpi),
+                set_from = 'auto'
+            }
+        end
+    end
+end
+
+
 --- Get global or per-screen DPI value falling back to xrdb.
 -- @tparam[opt] integer|screen s The screen.
 -- @treturn number DPI value.
 function xresources.get_dpi(s)
     s = get_screen(s)
     if s then
-        return dpi_per_screen[s] or rounded_dpi(s.dpi)
+        if not dpi_per_screen[s] then
+            dpi_per_screen[s] = {
+                dpi = rounded_dpi(s.dpi),
+                set_from = 'auto'
+            }
+        end
+        return dpi_per_screen[s].dpi
     end
     if not xresources.dpi then
-        -- Might not be present when run under unit tests
-        if awesome and awesome.xrdb_get_value then
-            xresources.dpi = tonumber(awesome.xrdb_get_value("", "Xft.dpi"))
-        end
-        -- Following Keith Packard's whitepaper on Xft,
-        -- https://keithp.com/~keithp/talks/xtc2001/paper/xft.html#sec-editing
-        -- the proper fallback for Xft.dpi is the vertical DPI reported by
-        -- the X server. This will generally be 96 on Xorg, unless the user
-        -- has configured it differently
-        if not xresources.dpi then
-            if root then
-                local mm_to_inch = 25.4
-                local _, h = root.size()
-                local _, hmm = root.size_mm()
-                if hmm ~= 0 then
-                    xresources.dpi = rounded_dpi(h*mm_to_inch/hmm)
-                end
-            end
-        end
-        -- ultimate fallback
-        if not xresources.dpi then
-            xresources.dpi = 96
-        end
+        set_global_dpi()
     end
-    return xresources.dpi
+    return xresources.dpi.dpi
 end
 
 
@@ -123,10 +167,11 @@ end
 -- @tparam[opt] integer s Screen.
 function xresources.set_dpi(dpi, s)
     s = get_screen(s)
+    local dpi_spec = { dpi = dpi, set_from = 'user' }
     if not s then
-        xresources.dpi = dpi
+        xresources.dpi = dpi_spec
     else
-        dpi_per_screen[s] = dpi
+        dpi_per_screen[s] = dpi_spec
     end
 end
 
@@ -137,6 +182,7 @@ end
 -- @tparam function a function to be applied to the DPI scaling factor
 function xresources.set_dpi_rounding(func)
     dpi_scale_rounding = func
+    recalc_auto_dpi()
 end
 
 
